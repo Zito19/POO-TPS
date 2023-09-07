9c5417a2dc693672b72c59e2d07a62fb5fb28bd6
Enzo <88214468+Zito19@users.noreply.github.com>
2023-09-06 23:02:14 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import TP1.Clases.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        ListaEnlazada lista = new ListaEnlazada();
+        lista.agregar("hola");
+        lista.agregar("chau");
+        lista.agregar("aceptame");
+        System.out.println(lista);
+    }
+}
